5f619bce746d0597466a143a38b91be37d32a265
mjmeans <mjmeans@ultrasw.com>
2023-07-06 21:41:57 -0700

    Create Write-Log.ps1

---
+++ Write-Log.ps1
@@ -0,0 +1,27 @@
+﻿
+Function Write-Log {
+    [CmdletBinding()]
+    Param(
+		[Parameter(Position=0,Mandatory=$False)]
+		[string]
+		$Message,
+		
+	    [Parameter(Mandatory=$False)]
+		[ValidateSet("INFO","WARN","ERROR","FATAL","DEBUG")]
+		[String]
+		$Level = "INFO",
+
+		[Parameter(Mandatory=$False)]
+		[string]
+		$logfile
+    )
+
+    $Stamp = (Get-Date).toString("yyyyMMdd_HHmmss.fff")
+    $Line = "$Stamp`: $Level`: $Message"
+    If($logfile) {
+        Add-Content $logfile -Value $Line
+    }
+    Else {
+        Write-Output $Line
+    }
+}
